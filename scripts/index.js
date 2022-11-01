44e52a1df2a9bd210d865840438e4f26e8c24d76
Chen <wengwei@ip-192-168-79-2.ap-southeast-3.compute.internal>
2022-11-01 21:43:54 +0800

    Added UI basic elements

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
+(function () {
+  "use strict";
 
+  const $elem = (selector) => document.querySelector(selector);
+
+  const init = () => {
+    $elem("#btnClickMe").addEventListener("click", () =>
+      console.log("Clicking")
+    );
+  };
+
+  window.addEventListener("load", () => {
+    init();
+  });
+})();
