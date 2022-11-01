b9f31664f1f241ad939ebaee8fc7f134a2712ea8
Chen <wengwei@Wengs-MacBook-Pro.local>
2022-11-01 22:04:04 +0800

    Implemented a basic functional version

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,35 @@
 
   const $elem = (selector) => document.querySelector(selector);
 
+  const CssUtils = {
+    addClass: (elem, className) => elem.classList.add(className),
+    removeClass: (elem, className) => elem.classList.remove(className),
+  };
+
+  let modalOverlay, messageContent;
+
+  const wireEvents = () => {
+    $elem("#btnClickMe").addEventListener("click", () => {
+      CssUtils.removeClass(modalOverlay, "modal-overlay--hidden");
+      messageContent.innerHTML = "";
+    });
+
+    $elem(".js-btnYes").addEventListener("click", () => {
+      CssUtils.addClass(modalOverlay, "modal-overlay--hidden");
+      messageContent.innerHTML = `You just clicked "Yes"`;
+    });
+
+    $elem(".js-btnCancel").addEventListener("click", () => {
+      CssUtils.addClass(modalOverlay, "modal-overlay--hidden");
+      messageContent.innerHTML = `You just clicked "Cancel"`;
+    });
+  };
+
   const init = () => {
-    $elem("#btnClickMe").addEventListener("click", () =>
-      console.log("Clicking")
-    );
+    modalOverlay = $elem(".js-modalOverlay");
+    messageContent = $elem(".js-message");
+
+    wireEvents();
   };
 
   window.addEventListener("load", () => {
